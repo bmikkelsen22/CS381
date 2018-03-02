1e00a6cc9334d357b73908b15103e443eb9830ee
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-03-01 22:15:38 -0800

    fixed issues causing tests to fail

--- assign4/KarelSemantics.hs
+++ assign4/KarelSemantics.hs
@@ -25,30 +25,37 @@ stmt PickBeeper _ w r = let p = getPos r
 stmt Move _ w r = let newPos = neighbor (getFacing r) (getPos r) in
       if (isClear (newPos) w)
             then OK w (newPos, (getFacing r), (getBag r))
-            else Error (show newPos ++ " is out of bounds")
+            else Error ("Blocked at: " ++ show newPos)
 stmt PutBeeper _ w r = let p = getPos r in
       if not (isEmpty r)
             then OK (incBeeper p w) (decBag r)
-            else Error "No beepers in bag to place"
+            else Error "No beeper to put."
 stmt (Turn d) _ w r = let c = getFacing r in
       OK w (setFacing (cardTurn d c) r)
 stmt (Block (h : t)) d w r = case (stmt h d w r) of
       (OK (w1) (r1)) -> stmt (Block t) d w1 r1
       (Error e) -> Error e
       (Done r) -> Done r
+stmt (Block []) d w r = OK w r
 stmt (If q t f) d w r = if (test q w r)
                         then stmt t d w r
                         else stmt f d w r
 stmt (Call m) d w r = let optS = (lookup m d) in
       case optS of 
             Just s -> stmt s d w r
-            Nothing -> Error ("Could not find macro " ++ m)
-stmt (Iterate i s) d w r = case i of
-      0 -> []
-      num -> (stmt s d w r) ++ (stmt (Iterate (num - 1) s) d w r)
-
--- stmt (While t s) d w r = if (test t w r)
---                          then stmt (While t s)
+            Nothing -> Error ("Undefined macro: " ++ m)
+stmt (Iterate i s) d w r = if i > 0
+      then case (stmt s d w r) of
+            (OK (w1) (r1)) ->  stmt (Iterate (i - 1) s) d w1 r1
+            (Error e) -> Error e
+            (Done r) -> Done r 
+      else (OK (w) (r))
+stmt (While t s) d w r = if (test t w r)
+      then case (stmt s d w r) of
+            (OK (w1) (r1)) -> stmt (While t s) d w1 r1
+            (Error e) -> Error e
+            (Done r) -> Done r
+      else OK (w) (r)
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
