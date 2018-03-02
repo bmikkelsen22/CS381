e1536656baafac052a4aba01ac3279493a63905c
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-03-01 21:50:06 -0800

    working on a4

--- assign4/KarelSemantics.hs
+++ assign4/KarelSemantics.hs
@@ -10,10 +10,10 @@ import KarelState
 -- | Valuation function for Test.
 test :: Test -> World -> Robot -> Bool
 test (Not t) w r = not (test t w r)
-test (Facing c) w (rp rc _) = rc == c
+test (Facing c) w r = (getFacing r) == c
 test (Clear d) w r = isClear (relativePos d r) w
 test Beeper w r = hasBeeper (getPos r) w
-test Empty w (_ _ c) = c == 0
+test Empty w r = isEmpty r
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
@@ -22,9 +22,9 @@ stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
                               else Error ("No beeper to pick at: " ++ show p)
-stmt Move _ w (rp rc ri) = let newPos = neighbor rc rp in
+stmt Move _ w r = let newPos = neighbor (getFacing r) (getPos r) in
       if (isClear (newPos) w)
-            then OK w (newPos rc ri)
+            then OK w (newPos, (getFacing r), (getBag r))
             else Error (show newPos ++ " is out of bounds")
 stmt PutBeeper _ w r = let p = getPos r in
       if not (isEmpty r)
@@ -32,7 +32,23 @@ stmt PutBeeper _ w r = let p = getPos r in
             else Error "No beepers in bag to place"
 stmt (Turn d) _ w r = let c = getFacing r in
       OK w (setFacing (cardTurn d c) r)
-    
+stmt (Block (h : t)) d w r = case (stmt h d w r) of
+      (OK (w1) (r1)) -> stmt (Block t) d w1 r1
+      (Error e) -> Error e
+      (Done r) -> Done r
+stmt (If q t f) d w r = if (test q w r)
+                        then stmt t d w r
+                        else stmt f d w r
+stmt (Call m) d w r = let optS = (lookup m d) in
+      case optS of 
+            Just s -> stmt s d w r
+            Nothing -> Error ("Could not find macro " ++ m)
+stmt (Iterate i s) d w r = case i of
+      0 -> []
+      num -> (stmt s d w r) ++ (stmt (Iterate (num - 1) s) d w r)
+
+-- stmt (While t s) d w r = if (test t w r)
+--                          then stmt (While t s)
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
